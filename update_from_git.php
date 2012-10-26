f67342b5d6966ed67485590fd51a04ae6ecf4263
YetiTheFoot <reg.yeti@gmail.com>
2012-10-26 17:01:26 +0300

    updated site update script

--- update_from_git.php
+++ update_from_git.php
@@ -1,10 +1,5 @@
 <?php 
-$output = shell_exec('git pull');
-echo "shell_exec: <pre>$output</pre>"; 
+$output = shell_exec('git help');
+echo "<pre>$output</pre>"; 
 
-$output = exec('git pull');
-echo "exec: <pre>$output</pre>"; 
-
-$output = system('git pull', $a);
-echo "exec: <pre>$output + $a</pre>"; 
 ?>
